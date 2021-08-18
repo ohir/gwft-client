69409a2c5a253a57d6062d1c61419d77cd7dd392
ohir <ohir@fairbe.org>
2021-08-18 19:53:07 +0200

    make MIT for public access

---
+++ go.mod
@@ -0,0 +1,8 @@
+module example.com/mC
+
+go 1.16
+
+require example.com/mP v0.1.2 // protocol
+
+replace example.com/mP => github.com/ohir/gwft-protocol v0.1.2
+// replace example.com/mP => ../protocol
---
+++ modC.go
@@ -0,0 +1,13 @@
+package Client
+
+import (
+	proto "example.com/mP"
+)
+
+const me string = "/mC Client"
+
+func Say() string {
+	return me[3:] +
+		" said: tinker here.\n\tThen: " +
+		proto.Say()
+}
